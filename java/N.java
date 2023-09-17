2c159b85679f93af7ccb2c0f49fc39856799dace
nithi2003 <105337794+nithi2003@users.noreply.github.com>
2023-09-17 22:15:00 +0530

    Update N.java

--- java/N.java
+++ java/N.java
@@ -2,5 +2,6 @@ public class N{
     public static void main(String args[]) {
           System.out.println("Hello Nithin!");
           System.out.println("Hello changed");
+          System.out.println("3rd changed");
 }
 }
