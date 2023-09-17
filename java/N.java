44d41b0e5dfc14e258f3057b5c554a098e9c2e30
nithi2003 <105337794+nithi2003@users.noreply.github.com>
2023-09-17 22:21:48 +0530

    Update N.java

--- java/N.java
+++ java/N.java
@@ -1,9 +1,7 @@
 public class N{
     public static void main(String args[]) {
-          System.out.println("Hello Nithin!");
-          System.out.println("Hello changed");
-          System.out.println("3rd changed");
-          System.out.println("4th changed");
+            System.out.println("Nithin pipeline!");
+
         
 }
 }
