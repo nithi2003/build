78cdc77b8efc22c8f1fde101a5afdc5d48ae8c0d
nithi2003 <105337794+nithi2003@users.noreply.github.com>
2023-09-18 08:30:09 +0530

    Create nithin.cpp

---
+++ cpp/nithin.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+// Main() function: where the execution of
+// program begins
+int main()
+{
+    // Prints hello world
+    int a=5;
+    cout <<a;
+ 
+    return 0;
+}
