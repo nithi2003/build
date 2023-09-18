8162a9dc63b50a16f868542ce87be64d0c87b54d
nithi2003 <105337794+nithi2003@users.noreply.github.com>
2023-09-18 08:46:27 +0530

    Update N.java

--- java/N.java
+++ java/N.java
@@ -1,7 +1,6 @@
 public class N{
     public static void main(String args[]) {
-            System.out.println("nto hcnaged");
-            System.out.println("Hello");
+            cout ,,
 
         
 }
